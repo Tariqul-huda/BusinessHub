5bd7881d4a8273fb7857b5ab43b7aaea06c26f96
Tariqul Huda <tariqulhuda6@gmail.com>
2025-12-22 01:27:19 +0600

    updated the table

--- Frontend/src/Utility/Tb.jsx
+++ Frontend/src/Utility/Tb.jsx
@@ -1,12 +1,55 @@
 import { motion } from "framer-motion";
-
+import Button from "./Button";
+import { useState } from "react";
 const Tb = ({content}) => {
     const head = Object.keys(content[0]);
 
     const values = content.map(el => Object.values(el));
     // console.log(values);
-
+    const [isOpen, setOpen] = useState(false)
+    const handleDemo = ()=>{
+        setOpen(false);
+    }
     return (
+        <>
+        <div className="flex justify-end">
+        <Button type="add" description="Add product" onClick = {()=>setOpen(true)}/>
+        </div>
+        {isOpen &&
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      className="bg-white p-8 rounded-xl shadow-2xl max-w-sm w-full relative"
+    >
+        
+        <form method="POST">
+            {head
+            .filter((el) => el !== "ID") 
+            .map((el, index) => (
+                <input 
+                key={index} 
+                placeholder={`Enter ${el}`} 
+                type="text" 
+                className="p-2 border rounded"
+                />
+            ))
+            }
+
+        </form>
+      
+      <button 
+        onClick={handleDemo}
+        className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+      >
+        Close Modal
+      </button>
+    </motion.div>
+  </div>
+        }
+        
+        {/* Main table starts from here  */}
+
         <div className="flex flex-col">
             <div className="flex flex-row font-bold bg-amber-100">
                 {head.map((el, index) => (
@@ -23,7 +66,6 @@ const Tb = ({content}) => {
                 ))}
             </div>
 
-            {/* 2. Table Data Rows */}
             <div className="flex flex-col">
                 {values.map((rowValues, rowIndex) => (
                     
@@ -49,6 +91,8 @@ const Tb = ({content}) => {
                 ))}
             </div>
         </div>
+        {/* Main table ends here */}
+        </>
     );
 }
 
